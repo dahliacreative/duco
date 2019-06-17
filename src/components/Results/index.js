d95a83ee6dfbe26505fd1f1d8d0732def3c3dfed
dahliacreative <simon@dahliacreative.com>
2019-06-17 21:54:57 +0100

    Moving context to lower level component

--- src/components/Results/index.js
+++ src/components/Results/index.js
@@ -1,18 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import Context from "../../data/characters";
 
-const Results = ({ data }) => (
-  <ul>
-    {data.map(character => {
-      const parts = character.url.split("/");
-      const id = parts[parts.length - 2];
-      return (
-        <li key={id}>
-          <Link to={`/${id}`}>{character.name}</Link>
-        </li>
-      );
-    })}
-  </ul>
-);
+const Results = () => {
+  const data = useContext(Context);
+  return (
+    <ul>
+      {data.map(character => {
+        const parts = character.url.split("/");
+        const id = parts[parts.length - 2];
+        return (
+          <li key={id}>
+            <Link to={`/${id}`}>{character.name}</Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
 
 export default Results;
--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,15 +1,12 @@
-import React, { useContext } from "react";
-import Context from "../../data/characters";
+import React from "react";
 import Input from "../Input";
 import Results from "../Results";
 
 const Search = ({ updateResults }) => {
-  const results = useContext(Context);
-  console.log(results);
   return (
     <div>
       <Input updateResults={updateResults} />
-      <Results data={results} />
+      <Results />
     </div>
   );
 };
